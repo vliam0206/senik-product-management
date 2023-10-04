641e91a0d2db0a138ade2480f1bcdc4dc655a0b5
viliam0206 <v.trclam@gmail.com>
2023-10-04 23:32:52 +0700

    add initial data

--- Domain/Product.cs
+++ Domain/Product.cs
@@ -8,6 +8,7 @@ public class Product : BaseEntity
     public string? Image { get; set; }
     public double Price { get; set; }
     public int Quantity { get; set; }
+    public string? Description { get; set; }
     public ProductStatusEnum Status { get; set; } = ProductStatusEnum.Active;
     public CategoryEnum Category { get; set; }
 
